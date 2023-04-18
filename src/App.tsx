d6db0dcb7aaf58d6706382261928e05fe63d1078
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-04-18 15:23:00 +0100

    Add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import classes from "./App.module.css";
 import AgeDisplay from "./components/AgeDisplay/AgeDisplay";
+import Button from "./components/Button/Button";
 import Form from "./components/Form/Form";
 
 const App = () => {
 	return (
 		<div className={classes.wrapper}>
 			{/* <Form /> */}
+			<Button />
 			<AgeDisplay />
 		</div>
 	);
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,23 @@
-const Button = () => {}
+import classes from "./Button.module.css";
+
+const Button = () => {
+	return (
+		<div className={classes.wrapper}>
+			<span className={classes.line}></span>
+			<button className={classes.button}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="46"
+					height="44"
+					viewBox="0 0 46 44"
+				>
+					<g fill="none" stroke="#FFF" stroke-width="2">
+						<path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
+					</g>
+				</svg>
+			</button>
+		</div>
+	);
+};
 
 export default Button;
