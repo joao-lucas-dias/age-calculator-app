50b6ffbbab41ee97cc025a48cdfbd825c1d0eeb7
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-04-18 12:04:08 +0100

    Add base code

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<span>Day DD Month MM Year YYYY -- years -- months -- days</span>
+			<div className="attribution">
+				Challenge by
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+				. Coded by <a href="#">Your Name Here</a>.
+			</div>
+		</>
+	);
+}
+
+export default App;
